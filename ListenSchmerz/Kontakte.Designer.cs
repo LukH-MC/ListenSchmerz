fd2cfdc527d85c7d19bc7980afd8c43985ff0ccc
TechniccGamingF <technicbaerchen@gmail.com>
2024-02-22 11:13:51 +0100

    Revert "+ LoadImage Button Logic"
    This reverts commit 2729c0a00b9cbde6ca39b4c6b224f9a3d67a53b4.

--- ListenSchmerz/Kontakte.Designer.cs
+++ ListenSchmerz/Kontakte.Designer.cs
@@ -41,19 +41,15 @@
             this.tb_Mail = new System.Windows.Forms.TextBox();
             this.button_Speichern = new System.Windows.Forms.Button();
             this.button_suche = new System.Windows.Forms.Button();
-            this.pictureBox = new System.Windows.Forms.PictureBox();
-            this.button_imageLoad = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // lName
             // 
             this.lName.AutoSize = true;
             this.lName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.16F);
-            this.lName.Location = new System.Drawing.Point(38, 34);
-            this.lName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lName.Location = new System.Drawing.Point(51, 42);
             this.lName.Name = "lName";
-            this.lName.Size = new System.Drawing.Size(53, 20);
+            this.lName.Size = new System.Drawing.Size(68, 25);
             this.lName.TabIndex = 0;
             this.lName.Text = "Name";
             this.lName.Click += new System.EventHandler(this.lName_Click);
@@ -62,10 +58,9 @@
             // 
             this.lVorname.AutoSize = true;
             this.lVorname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.16F);
-            this.lVorname.Location = new System.Drawing.Point(38, 109);
-            this.lVorname.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lVorname.Location = new System.Drawing.Point(51, 134);
             this.lVorname.Name = "lVorname";
-            this.lVorname.Size = new System.Drawing.Size(76, 20);
+            this.lVorname.Size = new System.Drawing.Size(98, 25);
             this.lVorname.TabIndex = 1;
             this.lVorname.Text = "Vorname";
             this.lVorname.Click += new System.EventHandler(this.label1_Click);
@@ -74,46 +69,41 @@
             // 
             this.lAdresse.AutoSize = true;
             this.lAdresse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.16F);
-            this.lAdresse.Location = new System.Drawing.Point(38, 192);
-            this.lAdresse.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lAdresse.Location = new System.Drawing.Point(51, 236);
             this.lAdresse.Name = "lAdresse";
-            this.lAdresse.Size = new System.Drawing.Size(71, 20);
+            this.lAdresse.Size = new System.Drawing.Size(91, 25);
             this.lAdresse.TabIndex = 2;
             this.lAdresse.Text = "Adresse";
             this.lAdresse.Click += new System.EventHandler(this.label2_Click);
             // 
             // tb_Name
             // 
-            this.tb_Name.Location = new System.Drawing.Point(123, 37);
-            this.tb_Name.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Name.Location = new System.Drawing.Point(164, 46);
             this.tb_Name.Name = "tb_Name";
-            this.tb_Name.Size = new System.Drawing.Size(156, 20);
+            this.tb_Name.Size = new System.Drawing.Size(207, 22);
             this.tb_Name.TabIndex = 3;
             // 
             // tb_Vorname
             // 
-            this.tb_Vorname.Location = new System.Drawing.Point(123, 112);
-            this.tb_Vorname.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Vorname.Location = new System.Drawing.Point(164, 138);
             this.tb_Vorname.Name = "tb_Vorname";
-            this.tb_Vorname.Size = new System.Drawing.Size(156, 20);
+            this.tb_Vorname.Size = new System.Drawing.Size(207, 22);
             this.tb_Vorname.TabIndex = 4;
             // 
             // tb_Adresse
             // 
-            this.tb_Adresse.Location = new System.Drawing.Point(123, 195);
-            this.tb_Adresse.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Adresse.Location = new System.Drawing.Point(164, 240);
             this.tb_Adresse.Name = "tb_Adresse";
-            this.tb_Adresse.Size = new System.Drawing.Size(156, 20);
+            this.tb_Adresse.Size = new System.Drawing.Size(207, 22);
             this.tb_Adresse.TabIndex = 5;
             // 
             // lTelefon
             // 
             this.lTelefon.AutoSize = true;
             this.lTelefon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.16F);
-            this.lTelefon.Location = new System.Drawing.Point(322, 35);
-            this.lTelefon.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lTelefon.Location = new System.Drawing.Point(430, 43);
             this.lTelefon.Name = "lTelefon";
-            this.lTelefon.Size = new System.Drawing.Size(64, 20);
+            this.lTelefon.Size = new System.Drawing.Size(84, 25);
             this.lTelefon.TabIndex = 6;
             this.lTelefon.Text = "Telefon";
             this.lTelefon.Click += new System.EventHandler(this.label1_Click_1);
@@ -122,10 +112,9 @@
             // 
             this.lMail.AutoSize = true;
             this.lMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.16F);
-            this.lMail.Location = new System.Drawing.Point(322, 109);
-            this.lMail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lMail.Location = new System.Drawing.Point(430, 134);
             this.lMail.Name = "lMail";
-            this.lMail.Size = new System.Drawing.Size(57, 20);
+            this.lMail.Size = new System.Drawing.Size(73, 25);
             this.lMail.TabIndex = 7;
             this.lMail.Text = "E-Mail";
             this.lMail.Click += new System.EventHandler(this.label1_Click_2);
@@ -134,35 +123,31 @@
             // 
             this.lBild.AutoSize = true;
             this.lBild.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.16F);
-            this.lBild.Location = new System.Drawing.Point(322, 197);
-            this.lBild.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lBild.Location = new System.Drawing.Point(430, 242);
             this.lBild.Name = "lBild";
-            this.lBild.Size = new System.Drawing.Size(38, 20);
+            this.lBild.Size = new System.Drawing.Size(48, 25);
             this.lBild.TabIndex = 8;
             this.lBild.Text = "Bild";
             // 
             // tb_Telefon
             // 
-            this.tb_Telefon.Location = new System.Drawing.Point(402, 38);
-            this.tb_Telefon.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Telefon.Location = new System.Drawing.Point(536, 47);
             this.tb_Telefon.Name = "tb_Telefon";
-            this.tb_Telefon.Size = new System.Drawing.Size(156, 20);
+            this.tb_Telefon.Size = new System.Drawing.Size(207, 22);
             this.tb_Telefon.TabIndex = 9;
             // 
             // tb_Mail
             // 
-            this.tb_Mail.Location = new System.Drawing.Point(402, 111);
-            this.tb_Mail.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Mail.Location = new System.Drawing.Point(536, 137);
             this.tb_Mail.Name = "tb_Mail";
-            this.tb_Mail.Size = new System.Drawing.Size(156, 20);
+            this.tb_Mail.Size = new System.Drawing.Size(207, 22);
             this.tb_Mail.TabIndex = 10;
             // 
             // button_Speichern
             // 
-            this.button_Speichern.Location = new System.Drawing.Point(103, 346);
-            this.button_Speichern.Margin = new System.Windows.Forms.Padding(2);
+            this.button_Speichern.Location = new System.Drawing.Point(164, 426);
             this.button_Speichern.Name = "button_Speichern";
-            this.button_Speichern.Size = new System.Drawing.Size(64, 24);
+            this.button_Speichern.Size = new System.Drawing.Size(86, 29);
             this.button_Speichern.TabIndex = 11;
             this.button_Speichern.Text = "Speichern";
             this.button_Speichern.UseVisualStyleBackColor = true;
@@ -170,44 +155,18 @@
             // 
             // button_suche
             // 
-            this.button_suche.Location = new System.Drawing.Point(180, 346);
-            this.button_suche.Margin = new System.Windows.Forms.Padding(2);
+            this.button_suche.Location = new System.Drawing.Point(281, 426);
             this.button_suche.Name = "button_suche";
-            this.button_suche.Size = new System.Drawing.Size(99, 24);
+            this.button_suche.Size = new System.Drawing.Size(105, 29);
             this.button_suche.TabIndex = 12;
             this.button_suche.Text = "Daten suchen";
             this.button_suche.UseVisualStyleBackColor = true;
-            this.button_suche.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // pictureBox
-            // 
-            this.pictureBox.BackColor = System.Drawing.SystemColors.Window;
-            this.pictureBox.InitialImage = null;
-            this.pictureBox.Location = new System.Drawing.Point(402, 192);
-            this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(182, 182);
-            this.pictureBox.TabIndex = 13;
-            this.pictureBox.TabStop = false;
-            // 
-            // button_imageLoad
-            // 
-            this.button_imageLoad.BackColor = System.Drawing.SystemColors.Control;
-            this.button_imageLoad.Location = new System.Drawing.Point(300, 346);
-            this.button_imageLoad.Margin = new System.Windows.Forms.Padding(2);
-            this.button_imageLoad.Name = "button_imageLoad";
-            this.button_imageLoad.Size = new System.Drawing.Size(79, 24);
-            this.button_imageLoad.TabIndex = 14;
-            this.button_imageLoad.Text = "Bild laden";
-            this.button_imageLoad.UseVisualStyleBackColor = false;
-            this.button_imageLoad.Click += new System.EventHandler(this.button2_Click);
             // 
             // Kontakte
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 394);
-            this.Controls.Add(this.button_imageLoad);
-            this.Controls.Add(this.pictureBox);
+            this.ClientSize = new System.Drawing.Size(932, 477);
             this.Controls.Add(this.button_suche);
             this.Controls.Add(this.button_Speichern);
             this.Controls.Add(this.tb_Mail);
@@ -221,12 +180,10 @@
             this.Controls.Add(this.lAdresse);
             this.Controls.Add(this.lVorname);
             this.Controls.Add(this.lName);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MinimumSize = new System.Drawing.Size(716, 433);
+            this.MinimumSize = new System.Drawing.Size(950, 524);
             this.Name = "Kontakte";
             this.Text = "Kontakte";
             this.Load += new System.EventHandler(this.Kontakte_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -247,8 +204,6 @@
         private System.Windows.Forms.TextBox tb_Mail;
         private System.Windows.Forms.Button button_Speichern;
         private System.Windows.Forms.Button button_suche;
-        private System.Windows.Forms.PictureBox pictureBox;
-        private System.Windows.Forms.Button button_imageLoad;
     }
 }
 
--- ListenSchmerz/Kontakte.cs
+++ ListenSchmerz/Kontakte.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,34 +52,7 @@ namespace ListenSchmerz
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Contact save button
-        }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            //Image load button
-            var filePath = string.Empty;
-
-            OpenFileDialog OpenFileDialog = new OpenFileDialog();
-            OpenFileDialog.InitialDirectory = "c:\\Bilder";
-            OpenFileDialog.Filter = "Images(*.BMP;*.JPG;*.GIF, *.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
-            OpenFileDialog.RestoreDirectory = true;
-            if (OpenFileDialog.ShowDialog() == DialogResult.OK) {
-                //Get the path of specified file
-                filePath = OpenFileDialog.FileName;
-            }
-            try {
-                pictureBox.Image = Image.FromFile(filePath);
-            }
-            catch (ArgumentException)
-            {
-                MessageBox.Show("Es wurde keine Datei angegeben!", "Error", MessageBoxButtons.OK);
-            }
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            //Data search button
         }
     }
 }
